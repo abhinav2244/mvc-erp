04b2d2119c5675fd8e0c7bf056a95811e4e85801
Abhinav Chaudhari <abhinavchaudhary283@gmail.com>
2026-09-17 15:40:20 +0530

    update prograrm.cs file for repeatitive code

--- Program.cs
+++ Program.cs
@@ -5,7 +5,6 @@ using mvc_erp.Services.ExaminationDashboard;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 builder.Services.AddDbContext<ErpDbContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection")
@@ -38,27 +37,7 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-
-    builder.Services.AddDbContext<ErpDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-}
-// Register LoginService.
-// ASP.NET Core will create one instance per HTTP request.
-builder.Services.AddScoped<LoginService>();
-builder.Services.AddScoped<ExaminationDashboardService>();
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-
     app.UseHsts();
 }
 
@@ -66,13 +45,10 @@ app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 
-
 app.UseRouting();
 
 app.UseSession();
 
-app.UseRouting();
-app.UseSession();
 app.UseAuthorization();
 
 app.MapStaticAssets();
@@ -82,8 +58,5 @@ app.MapControllerRoute(
     pattern: "{controller=Account}/{action=Login}/{id?}"
 )
 .WithStaticAssets();
-
-app.Run();
-app.MapControllerRoute(name: "default", pattern: "{controller=Account}/{action=Login}/{id?}").WithStaticAssets();
 
 app.Run();
